b2b5d4fbc1aa854c22505d0724d1029f27b121f6
Brendan McGuire <brendan@bren.app>
2023-11-27 23:35:51 -0600

    Allow consumers to specify event options

--- src/utils/hooks/robotevents.ts
+++ src/utils/hooks/robotevents.ts
@@ -5,6 +5,7 @@ import { Year } from "robotevents/out/endpoints/seasons";
 import { ProgramAbbr } from "robotevents/out/endpoints/programs";
 
 import * as rules from "../../assets/rules.json";
+import { TeamOptionsFromEvent } from "robotevents/out/endpoints/teams";
 
 const ROBOTEVENTS_TOKEN =
     "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIzIiwianRpIjoiYjM5Y2I1NGNhMTk0OTM0ODNmNTc0MDQ2MTRhZDY0MDZjYTY1ZmQzMjAzNDlhMmM5YmUwOThlNmJjNzhhZWJmZmZjYzU0ZWY2MTQ2ZmQyYjEiLCJpYXQiOjE2ODc2NDIzODcuNTUwMjg4LCJuYmYiOjE2ODc2NDIzODcuNTUwMjkxMSwiZXhwIjoyNjM0NDE3MTg3LjUzNzIzNjIsInN1YiI6Ijk3MDY5Iiwic2NvcGVzIjpbXX0.k0DEt3QRKkgZnyV8X9mDf6VYyc8aOsIEfQbVN4Gi6Csr7O5ILLGFENXZouvplqbcMDdQ8gBMMLg5hIR38RmrTsKcWHMndq1T8wYkGZQfRhc_uZYLQhGQCaanf_F_-gnKocFwT1AKQJmAPkAbV-Itb2UzHeGpNuW8vV_TaNL3coaYvmM6rubwBuNYgyZhTHW_Mgvzh5-XBqqGpmQLm9TGl4gkeqnS-6a5PfoqRTc8v3CQWSCURFry5BA2oXz0lcWmq92FY5crr2KKv1O3chPr--oMba97elY0y9Dw0q2ipKcTm4pE7bbFP8t7-a_RKU4OyXuHRIQXjw3gEDCYXY5Hp22KMY0idnRIPhat6fybxcRfeyzUzdnubRBkDMNklwlgNCyeu2ROqEOYegtu5727Wwvy2I-xW-ZVoXg0rggVu7jVq6zmBqDFIcu50IS9R4P6a244pg2STlBaAGpzT2VfUqCBZrbtBOvdmdNzxSKIkl1AXeOIZOixo1186PX54p92ehXfCbcTgWrQSLuAAg_tBa6T7UFKFOGecVFo3v0vkmE__Q5-701f1qqcdDRNlOG-bzzFh9QLEdJWlpEajwYQ1ZjTAlbnBpKy3IrU0Aa-Jr0aqxtzgr5ZlghNtOcdYYRw5_BN0BOMmAnkvtm0_xzIJSsFbWJQJ8QpPk_n4zKZf-Y";
@@ -33,17 +34,19 @@ export function useTeam(numberOrID: string | number, program?: ProgramAbbr) {
 }
 
 export function useEventTeams(
-    event: robotevents.events.Event | null | undefined
+    event: robotevents.events.Event | null | undefined,
+    options?: TeamOptionsFromEvent
 ): UseQueryResult<robotevents.teams.Team[]> {
-    return useQuery(["teams", event?.sku], async () => {
+    return useQuery(["teams", event?.sku, options], async () => {
         if (!event) {
             return [];
         }
 
-        const teams = await event.teams({ registered: true });
+        const teams = await event.teams({ registered: true, ...options });
         return teams.array();
     });
 }
+
 
 export function logicalMatchComparison(a: Match, b: Match) {
 

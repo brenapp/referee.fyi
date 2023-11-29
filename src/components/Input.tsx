98dcd392629bd392e2f4ec4c008dfe24f5694612
Brendan McGuire <brendan@bren.app>
2023-11-28 20:39:12 -0600

    Rules Select Component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,5 +1,7 @@
 import { twMerge } from "tailwind-merge";
 import { HTMLProps } from "../utils/types";
+import { Game, Rule } from "../utils/hooks/robotevents";
+import { useCallback } from "react";
 
 export type InputBaseProps = HTMLProps<HTMLInputElement>;
 
@@ -8,6 +10,23 @@ export type InputProps = InputBaseProps & {};
 export const Input: React.FC<InputProps> = (props) => {
   return (
     <input
+      {...props}
+      className={twMerge(
+        "rounded-md bg-zinc-700 text-zinc-100 text-left px-3 py-2",
+        "hover:bg-zinc-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-zinc-500",
+        props.className
+      )}
+    />
+  );
+};
+
+export type TextAreaBaseProps = HTMLProps<HTMLTextAreaElement>;
+
+export type TextAreaProps = TextAreaBaseProps & {};
+
+export const TextArea: React.FC<TextAreaProps> = (props) => {
+  return (
+    <textarea
       {...props}
       className={twMerge(
         "rounded-md bg-zinc-700 text-zinc-100 text-left px-3 py-2",
@@ -37,5 +56,57 @@ export const Select: React.FC<SelectBaseProps> = ({ children, ...props }) => {
     >
       {children}
     </select>
+  );
+};
+
+export type RulesSelectProps = SelectBaseProps & {
+  game?: Game;
+  rule: Rule | null;
+  setRule: (rule: Rule | null) => void;
+};
+
+export const RulesSelect: React.FC<RulesSelectProps> = ({
+  game,
+  rule,
+  setRule,
+  ...props
+}) => {
+  const onChangeRule = useCallback(
+    (e: React.ChangeEvent<HTMLSelectElement>) => {
+      const group = e.target.selectedOptions[0].dataset.rulegroup;
+      if (!group) return;
+
+      const rule = game?.ruleGroups
+        .find((g) => g.name === group)
+        ?.rules.find((r) => r.rule === e.target.value);
+
+      if (!rule) return;
+      setRule(rule);
+    },
+    []
+  );
+
+  return (
+    <Select
+      className="w-full py-4"
+      value={rule?.rule}
+      onChange={onChangeRule}
+      {...props}
+    >
+      <option>Pick A Rule</option>
+      {game?.ruleGroups.map((group) => (
+        <optgroup label={group.name} key={group.name}>
+          {group.rules.map((rule) => (
+            <option
+              value={rule.rule}
+              data-rulegroup={group.name}
+              key={rule.rule}
+            >
+              {rule.rule}
+            </option>
+          ))}
+        </optgroup>
+      ))}
+    </Select>
   );
 };
--- src/pages/rules/program.tsx
+++ src/pages/rules/program.tsx
@@ -1,6 +1,6 @@
-import { useCallback, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { Rule, useRulesForProgram } from "../../utils/hooks/robotevents";
-import { Select } from "../../components/Input";
+import { RulesSelect } from "../../components/Input";
 import { Navigate, useParams } from "react-router-dom";
 import { ProgramAbbr } from "robotevents/out/endpoints/programs";
 
@@ -15,43 +15,9 @@ export const RulesProgramPage: React.FC = () => {
 
   const [rule, setRule] = useState<Rule | null>(null);
 
-  const onChangeRule = useCallback(
-    (e: React.ChangeEvent<HTMLSelectElement>) => {
-      const group = e.target.selectedOptions[0].dataset.rulegroup;
-      if (!group) return;
-
-      const rule = gameRules.ruleGroups
-        .find((g) => g.name === group)
-        ?.rules.find((r) => r.rule === e.target.value);
-
-      if (!rule) return;
-      setRule(rule);
-    },
-    []
-  );
-
   return (
     <section className="mt-4">
-      <Select
-        className="w-full py-4"
-        value={rule?.rule}
-        onChange={onChangeRule}
-      >
-        <option>Pick A Rule</option>
-        {gameRules.ruleGroups.map((group) => (
-          <optgroup label={group.name} key={group.name}>
-            {group.rules.map((rule) => (
-              <option
-                value={rule.rule}
-                data-rulegroup={group.name}
-                key={rule.rule}
-              >
-                {rule.rule}
-              </option>
-            ))}
-          </optgroup>
-        ))}
-      </Select>
+      <RulesSelect game={gameRules} rule={rule} setRule={setRule} />
       <div className="h-full bg-zinc-100 rounded-md mt-4">
         <iframe
           ref={ref}

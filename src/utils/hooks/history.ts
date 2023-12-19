a8de0a301bfa682acb8e9f006376d787ceeb4c51
Brendan McGuire <brendan@bren.app>
2023-12-18 20:43:24 -0600

    Display recent rules

--- src/utils/hooks/history.ts
+++ src/utils/hooks/history.ts
@@ -5,11 +5,17 @@
 import { get, set } from "idb-keyval";
 import { useMutation, useQuery } from "react-query";
 import { EventData, Event } from "robotevents/out/endpoints/events";
+import { Rule } from "./rules";
 
 export async function initHistoryStore() {
-    const incidents = await get<EventData[]>("event_history");
-    if (!incidents) {
+    const events = await get<EventData[]>("event_history");
+    if (!events) {
         await set("event_history", []);
+    }
+
+    const rules = await get<Rule[]>("rule_history");
+    if (!rules) {
+        await set("rule_history", []);
     }
 };
 
@@ -17,6 +23,10 @@ export async function getRecentEvents() {
     return (await get<EventData[]>("event_history")) ?? [];
 };
 
+export async function getRecentRules() {
+    return (await get<Rule[]>("rule_history")) ?? [];
+
+}
 
 export function useRecentEvents(limit?: number) {
     return useQuery("recent_events", async () => {
@@ -24,6 +34,13 @@ export function useRecentEvents(limit?: number) {
 
         return events.slice(0, limit);
     });
+};
+
+export function useRecentRules(limit?: number) {
+    return useQuery("recent_rules", async () => {
+        const rules = await getRecentRules();
+        return rules.slice(0, limit);
+    }, { cacheTime: 0 });
 };
 
 export function useAddEventVisited() {
@@ -47,6 +64,19 @@ export function useAddEventVisited() {
 
             const events = (await getRecentEvents()).filter(e => e.sku !== event.sku)
             await set("event_history", [data, ...events])
+        }
+    )
+}
+
+export function useAddRecentRules() {
+    return useMutation(
+        async (rules: Rule[]) => {
+            if (rules.length < 1) {
+                return;
+            }
+
+            const recent = (await getRecentRules()).filter(a => rules.every(b => b.rule !== a.rule))
+            await set("rule_history", [...rules, ...recent])
         }
     )
 }

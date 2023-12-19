f56c7814b705f07d199100ec5791919a85045a4a
Brendan McGuire <brendan@bren.app>
2023-12-18 21:10:46 -0600

    Allow users to remove incidents

--- src/pages/events/team.tsx
+++ src/pages/events/team.tsx
@@ -6,15 +6,22 @@ import {
   useTeam,
 } from "~hooks/robotevents";
 import { Spinner } from "~components/Spinner";
-import { useMemo } from "react";
+import { useCallback, useMemo } from "react";
 import { useCurrentEvent } from "~hooks/state";
 import { useTeamIncidentsByEvent } from "~hooks/incident";
-import { IncidentOutcome, IncidentWithID } from "~utils/data/incident";
+import {
+  IncidentOutcome,
+  IncidentWithID,
+  deleteIncident,
+} from "~utils/data/incident";
 import { twMerge } from "tailwind-merge";
 import { Tabs } from "~components/Tabs";
 import { Event } from "robotevents/out/endpoints/events";
 import { Team } from "robotevents/out/endpoints/teams";
 import { ClickableMatch } from "~components/ClickableMatch";
+import { IconButton } from "~components/Button";
+import { TrashIcon } from "@heroicons/react/24/outline";
+import { queryClient } from "~utils/data/query";
 
 type EventTeamsTabProps = {
   event: Event | null | undefined;
@@ -41,9 +48,9 @@ const IncidentOutcomeClasses: { [O in IncidentOutcome]: string } = {
 
 export type IncidentProps = {
   incident: IncidentWithID;
-};
+} & React.HTMLProps<HTMLDivElement>;
 
-export const Incident: React.FC<IncidentProps> = ({ incident }) => {
+export const Incident: React.FC<IncidentProps> = ({ incident, ...props }) => {
   const { data: event } = useEvent(incident.event);
   const { data: match } = useEventMatch(
     event,
@@ -51,26 +58,44 @@ export const Incident: React.FC<IncidentProps> = ({ incident }) => {
     incident.match
   );
 
+  const onRemove = useCallback(async () => {
+    await deleteIncident(incident.id);
+    queryClient.invalidateQueries({
+      queryKey: ["incidents"],
+    });
+  }, []);
+
   return (
     <div
+      {...props}
       className={twMerge(
         IncidentOutcomeClasses[incident.outcome],
-        "px-4 py-2 rounded-md mt-2"
+        "px-4 py-2 rounded-md mt-2 flex",
+        props.className
       )}
     >
-      <p className="text-sm">
-        {[incident.team, match?.name, IncidentOutcome[incident.outcome]].join(
-          " • "
-        )}
-      </p>
-      <p>{incident.notes}</p>
-      <ul>
-        {incident.rules.map((r) => (
-          <li key={r} className="text-sm font-mono">
-            {r}
-          </li>
-        ))}
-      </ul>
+      <div className="flex-1">
+        <p className="text-sm">
+          {[
+            incident.team,
+            match?.name ?? "Event-Wide",
+            IncidentOutcome[incident.outcome],
+          ].join(" • ")}
+        </p>
+        <p>{incident.notes}</p>
+        <ul>
+          {incident.rules.map((r) => (
+            <li key={r} className="text-sm font-mono">
+              {r}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <IconButton
+        className="bg-transparent text-inherit"
+        onClick={onRemove}
+        icon={<TrashIcon height={20} />}
+      ></IconButton>
     </div>
   );
 };

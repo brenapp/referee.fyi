f4335eeaca880900b444aa5ba711cbd2816b4245
Brendan McGuire <brendan@bren.app>
2023-12-18 19:26:36 -0600

    use team specific reporting functionality

--- src/pages/events/dialogs/match.tsx
+++ src/pages/events/dialogs/match.tsx
@@ -2,7 +2,8 @@ import { useCurrentDivision, useCurrentEvent } from "~hooks/state";
 import { useEventMatch, useEventMatches } from "~hooks/robotevents";
 import { Button } from "~components/Button";
 import { ArrowLeftIcon } from "@heroicons/react/20/solid";
-import { ArrowRightIcon, FlagIcon } from "@heroicons/react/24/outline";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import { FlagIcon } from "@heroicons/react/20/solid";
 import { useCallback, useMemo, useState } from "react";
 import { twMerge } from "tailwind-merge";
 import { MatchContext } from "~components/Context";
@@ -17,6 +18,26 @@ import { useTeamIncidentsByMatch } from "~utils/hooks/incident";
 import { EventNewIncidentDialog } from "./new";
 import { IncidentOutcome } from "~utils/data/incident";
 import { shortMatchName } from "~utils/data/match";
+import { Team } from "robotevents/out/endpoints/teams";
+import * as robotevents from "robotevents";
+import { useQuery } from "react-query";
+import { Match } from "robotevents/out/endpoints/matches";
+
+export function useMatchTeams(match?: Match | null) {
+  return useQuery(["match_teams", match?.id], async () => {
+    if (!match) {
+      return null;
+    }
+
+    const teams = match.alliances
+      .map((alliance) => alliance.teams.map((t) => t.team.id))
+      .flat();
+
+    return Promise.all(
+      teams.map((id) => robotevents.teams.get(id) as Promise<Team>)
+    );
+  });
+}
 
 export type EventMatchDialogProps = {
   matchId: number;
@@ -38,6 +59,10 @@ export const EventMatchDialog: React.FC<EventMatchDialogProps> = ({
   const { data: matches } = useEventMatches(event, division);
   const { data: match } = useEventMatch(event, division, matchId);
 
+  const { data: matchTeams } = useMatchTeams(match);
+
+  const [initialTeam, setInitialTeam] = useState<Team | undefined>(undefined);
+
   const prevMatch = useMemo(() => {
     return matches?.find((_, i) => matches[i + 1]?.id === match?.id);
   }, [matches, match]);
@@ -58,6 +83,17 @@ export const EventMatchDialog: React.FC<EventMatchDialogProps> = ({
     }
   }, [nextMatch]);
 
+  const onClickTeam = useCallback(
+    async (number: string) => {
+      const team = matchTeams?.find((t) => t.number === number);
+      setInitialTeam(team ?? undefined);
+      setTimeout(() => {
+        setIncidentDialogOpen(true);
+      }, 0);
+    },
+    [matchTeams]
+  );
+
   const { data: incidentsByTeam } = useTeamIncidentsByMatch(match);
 
   const [incidentDialogOpen, setIncidentDialogOpen] = useState(false);
@@ -68,6 +104,7 @@ export const EventMatchDialog: React.FC<EventMatchDialogProps> = ({
         open={incidentDialogOpen}
         setOpen={setIncidentDialogOpen}
         initialMatch={match}
+        initialTeam={initialTeam}
       />
       <Dialog
         open={open}
@@ -106,13 +143,6 @@ export const EventMatchDialog: React.FC<EventMatchDialogProps> = ({
                   className="w-full"
                   allianceClassName="w-full"
                 />
-                <Button
-                  onClick={() => setIncidentDialogOpen(true)}
-                  className="w-full text-center bg-emerald-600 mt-4"
-                >
-                  <FlagIcon height={20} className="inline mr-2 " />
-                  New Entry
-                </Button>
                 <section className="mt-4">
                   <h2 className="text-center">Previous Violations</h2>
                   <div className="flex gap-2 mt-4 items-stretch">
@@ -124,9 +154,13 @@ export const EventMatchDialog: React.FC<EventMatchDialogProps> = ({
                       };
                       return (
                         <div className="flex-1 flex items-center flex-col rounded-md">
-                          <h3 className="font-mono text-center text-emerald-400">
+                          <Button
+                            className="font-mono text-center bg-emerald-600 mb-2"
+                            onClick={() => onClickTeam(team)}
+                          >
+                            <FlagIcon height={16} className="inline mr-2 " />
                             {team}
-                          </h3>
+                          </Button>
                           <ul className="text-center font-mono italic flex-1">
                             {incidents.length > 0 ? (
                               incidents.map((incident) => {

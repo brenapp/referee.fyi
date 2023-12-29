e9102f0d2a23cff9de865fd7cb0df8b831d47014
MiniMitre <34739837+MiniMitre@users.noreply.github.com>
2023-12-29 19:59:06 +0000

    Use dialog element & make .md public

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -10,11 +10,15 @@ export const HomePage: React.FC = () => {
 
   const [markdownContent, setMarkdownContent] = useState<string>("");
 
+  const dialog = document.getElementById(
+    "markdown-dialog"
+  ) as HTMLDialogElement;
+
   useEffect(() => {
     // Fetch the update notes as a markdown file
     const fetchMarkdownContent = async () => {
       try {
-        const response = await fetch("updateNotes.md");
+        const response = await fetch("/updateNotes.md");
         const content = await response.text();
         setMarkdownContent(content);
       } catch (error) {
@@ -28,52 +32,52 @@ export const HomePage: React.FC = () => {
     const userVersion = localStorage.getItem("version");
 
     if (userVersion && userVersion !== latestVersion) {
-      document.getElementById("markdown")?.classList.remove("hidden");
+      if (dialog) {
+        dialog.show();
+      }
     }
     localStorage.setItem("version", latestVersion);
   }, []); // Run this effect only once, when the component mounts
 
   return (
-    <main>
-      <aside className="text-right">
-        <button
-          className="bg-cyan-900 text-right ml-auto mt-2 py-1 px-3 rounded-md"
-          onClick={() =>
-            document.getElementById("markdown")?.classList.remove("hidden")
-          }
-        >
-          View Update Notes
-        </button>
-      </aside>
-
-      <section className="max-w-full">
-        {recentEvents?.map((event) => (
-          <LinkButton
-            to={`/${event.sku}`}
-            className="w-full max-w-full mt-4"
-            key={event.sku}
+    <>
+      <main>
+        <aside className="text-right">
+          <button
+            className="bg-cyan-900 text-right ml-auto mt-2 py-1 px-3 rounded-md"
+            onClick={() => dialog.show()}
           >
-            <p className="text-sm">
-              <span className=" text-emerald-400 font-mono">{event.sku}</span>
-            </p>
-            <p className="">{event.name}</p>
-          </LinkButton>
-        ))}
-      </section>
-      <div
-        className="hidden absolute top-8 left-1/2 rounded-md p-4 bg-neutral-800 -translate-x-1/2 w-11/12"
-        id="markdown"
+            View Update Notes
+          </button>
+        </aside>
+
+        <section className="max-w-full">
+          {recentEvents?.map((event) => (
+            <LinkButton
+              to={`/${event.sku}`}
+              className="w-full max-w-full mt-4"
+              key={event.sku}
+            >
+              <p className="text-sm">
+                <span className=" text-emerald-400 font-mono">{event.sku}</span>
+              </p>
+              <p className="">{event.name}</p>
+            </LinkButton>
+          ))}
+        </section>
+      </main>
+      <dialog
+        className="markdown absolute overflow-y-auto text-white absolute top-0 left-1/2 rounded-b-md p-4 bg-slate-800 -translate-x-1/2 w-11/12"
+        id="markdown-dialog"
       >
         <div
-          className="absolute top-2 right-2 cursor-pointer"
-          onClick={() =>
-            document.getElementById("markdown")?.classList.add("hidden")
-          }
+          className=" sticky w-fit top-0 ml-auto mr-1 p-2 bg-slate-800 cursor-pointer rounded-md -mt-12"
+          onClick={() => dialog.close()}
         >
           &#10006; {/* Unicode character for X */}
         </div>
-        <Markdown className="markdown">{markdownContent}</Markdown>
-      </div>
-    </main>
+        <Markdown>{markdownContent}</Markdown>
+      </dialog>
+    </>
   );
 };

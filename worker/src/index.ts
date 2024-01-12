62fe488a31116e1fdd316990c05c143462ad3d60
Brendan McGuire <brendan@bren.app>
2024-01-11 21:17:54 -0600

    Wrap in methods in try-catch

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -22,26 +22,34 @@ router
             })
         };
 
-        const body = await request.json() as EventIncidents;
+        try {
+            const body = await request.json() as EventIncidents;
 
-        const base = crypto.randomUUID();
-        const code = base.slice(0, 3).toUpperCase() + "-" + base.slice(3, 6).toUpperCase();
+            const base = crypto.randomUUID();
+            const code = base.slice(0, 3).toUpperCase() + "-" + base.slice(3, 6).toUpperCase();
 
-        await env.SHARES.put(`${sku}#${code}`, JSON.stringify(body));
+            await env.SHARES.put(`${sku}#${code}`, JSON.stringify(body));
 
-        return response<CreateShareResponse>({
-            success: true,
-            data: { code }
-        })
+            return response<CreateShareResponse>({
+                success: true,
+                data: { code }
+            });
 
+        } catch (e) {
+            return response({
+                success: false,
+                reason: "server_error",
+                details: `${e}`
+            })
+        };
     })
-    .all("/api/share/:sku/:code/:path+", (request, env: Env) => {
+    .all("/api/share/:sku/:code/:path+", async (request, env: Env) => {
         const sku = request.params.sku;
         const code = request.params.code;
 
-        console.log(sku, code);
+        const entry = await env.SHARES.get(`${sku}#${code}`);
 
-        if (!env.SHARES.get(`${sku}#${code}`)) {
+        if (!entry) {
             return response({
                 success: false,
                 reason: "incorrect_code",
@@ -53,7 +61,6 @@ router
         const share = env.INCIDENTS.get(id);
 
         const search = new URL(request.url).search;
-
 
         return share.fetch(`https://share/${request.params.path}${search}`, request)
     })

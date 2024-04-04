8fc2bf57686f1e61fe6268b8a9f86ac4acc988bd
Brendan McGuire <brendan@bren.app>
2024-04-03 22:46:36 -0500

    Add generate function to programmatically invite people

--- utils/upload/generate.js
+++ utils/upload/generate.js
@@ -1,25 +1,86 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import config from "./config.json" assert { type: "json" };
 import crypto from "crypto";
-import fs from "fs";
+import fs from "fs/promises";
+
+const values = await fs
+  .readFile("./headreferees.tsv")
+  .then((b) => b.toString("utf8"));
+
+const [header, ...users] = values.split(/\r*\n/g).slice(1);
+const skus = header.split("\t").slice(4);
 
 const invitations = [];
 
-for (const user of config.users) {
-  const id = crypto.randomUUID();
+for (const user of users) {
+  const [adminRaw, emailRaw, name, publicKey, ...events] = user.split("\t");
+  if (!name) continue;
 
-  const invitation = {
-    ...config.invitation,
-    id,
-    user,
-  };
+  console.log(name, emailRaw);
 
-  invitations.push({
-    key: `${user}#${config.invitation.sku}`,
-    value: JSON.stringify(invitation),
+  for (let i = 0; i < skus.length; i++) {
+    if (events[i] !== "TRUE") {
+      continue;
+    }
+
+    invitations.push({
+      key: `${publicKey}#${skus[i]}`,
+      value: {
+        id: crypto.randomUUID(),
+        sku: skus[i],
+        instance_secret: config.invitations[skus[i]],
+        user: publicKey,
+        from: config.from,
+        admin: adminRaw === "TRUE",
+        accepted: true,
+      },
+    });
+  }
+}
+
+await fs.writeFile(
+  "./invitations.json",
+  JSON.stringify(
+    invitations.map(({ key, value }) => ({
+      key,
+      value: JSON.stringify(value),
+    })),
+    null,
+    4
+  )
+);
+
+const shares = [];
+
+for (let i = 0; i < skus.length; i++) {
+  const sku = skus[i];
+  const secret = config.invitations[skus[i]];
+
+  const admins = invitations
+    .filter((inv) => inv.value.admin && inv.value.sku === sku)
+    .map((i) => i.value.user);
+  const invs = invitations
+    .filter((inv) => inv.value.sku === sku)
+    .map((i) => i.value.user);
+
+  console.log(sku, admins, invs);
+
+  shares.push({
+    key: `${sku}#${secret}`,
+    value: {
+      sku,
+      admins,
+      invitations: invs,
+      secret,
+    },
   });
 }
 
-fs.writeFileSync(
-  `invitations-${config.invitation.sku}.json`,
-  JSON.stringify(invitations, null, 4)
+await fs.writeFile(
+  "./shares.json",
+  JSON.stringify(
+    shares.map(({ key, value }) => ({ key, value: JSON.stringify(value) })),
+    null,
+    4
+  )
 );
---
+++ utils/upload/generate.sh
@@ -0,0 +1,4 @@
+node generate.js
+
+wrangler kv:bulk put shares.json --namespace-id 667b3f219f284aa7b4a88d37044ae9d9
+wrangler kv:bulk put invitations.json --namespace-id bf2c971507354390ba314b8e18b4e908

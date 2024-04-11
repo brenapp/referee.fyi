9aaf791205e7e3054d2235f06e4e9890bbec3951
MiniMitre <34739837+MiniMitre@users.noreply.github.com>
2024-04-11 20:05:18 +0100

    revert vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: {
-    "import.meta.env.VITE_REFEREE_FYI_BUILD_MODE": '"WC"',
-  },
   plugins: [
     react(),
     vitePluginVersionMark({

3924301a1b92d537879f337e13b3c1f5b8ea1ab0
Brendan McGuire <brendan@bren.app>
2024-07-16 21:58:47 -0500

    Migrate all incidents store to set

---
+++ src/migrations/2024_07_12_setIndicesAll.ts
@@ -0,0 +1,12 @@
+import { queueMigration } from "./utils";
+import { update } from "~utils/data/keyval";
+
+queueMigration({
+  name: `2024_07_12_setIndicesAll`,
+  run_order: 1,
+  dependencies: ["2024_05_17_splitIndex"],
+  apply: async () => {
+    await update<Set<string> | string[]>("incidents", (old) => new Set(old));
+    return { success: true };
+  },
+});

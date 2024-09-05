a3856b1b91d9d035491101a5566671a4d70d24b3
Brendan McGuire <brendan@bren.app>
2024-09-04 20:16:15 -0500

    Dynamically load sentry replay bundle

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -1,4 +1,4 @@
-import * as Sentry from "@sentry/react";
+import { init, browserTracingIntegration } from "@sentry/react";
 import { queryClient } from "./data/query";
 
 export async function clearCache() {
@@ -17,12 +17,9 @@ export async function clearCache() {
   window.location.reload();
 }
 
-Sentry.init({
+const client = init({
   dsn: "https://0aecdc6c41674e7cf3b4a39ec939ed9c@o4507708571910144.ingest.us.sentry.io/4507708573286400",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
+  integrations: [browserTracingIntegration()],
   environment: import.meta.env.MODE,
   enabled:
     import.meta.env.MODE === "production" ||
@@ -35,3 +32,10 @@ Sentry.init({
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
+
+// Load Replay Integration in a Separate Bundle
+if (import.meta.env.PROD) {
+  import("@sentry-internal/replay").then(({ replayIntegration }) => {
+    client?.addIntegration(replayIntegration);
+  });
+}

024e25293561c02e9af397e52175d8f65810e2e8
MiniMitre <34739837+MiniMitre@users.noreply.github.com>
2025-01-14 20:39:43 +0000

    Style the team number different from other inputs

--- src/components/dialogs/edit.tsx
+++ src/components/dialogs/edit.tsx
@@ -229,7 +229,7 @@ export const EditIncidentDialog: React.FC<EditIncidentDialogProps> = ({
       <DialogBody>
         <label>
           <p className="mt-4">Team</p>
-          <div className="rounded-md bg-zinc-700 text-zinc-100 text-left px-3 py-2 hover:bg-zinc-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-zinc-500 w-full">
+          <div className="rounded-md bg-zinc-700 text-zinc-100 text-left px-3 py-2 w-fit font-semibold">
             {incident.team}
           </div>
         </label>

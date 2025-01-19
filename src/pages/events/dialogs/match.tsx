b3b66d7c63915f21e273fa3dbf248f3c58cc94b4
MiniMitre <34739837+MiniMitre@users.noreply.github.com>
2025-01-19 23:02:27 +0000

    Fix edit button clipping with no incident notes

--- src/pages/events/dialogs/match.tsx
+++ src/pages/events/dialogs/match.tsx
@@ -151,7 +151,11 @@ const TeamSummary: React.FC<TeamSummaryProps> = ({
       {open ? (
         <>
           {incidents.map((incident) => (
-            <Incident incident={incident} key={incident.id} />
+            <Incident
+              className="h-14 overflow-hidden"
+              incident={incident}
+              key={incident.id}
+            />
           ))}
           {incidents.length > 0 ? (
             <>

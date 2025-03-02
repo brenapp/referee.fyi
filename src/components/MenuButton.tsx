279b66a54085235ca6cb8918a219b2fb86961f29
Brendan McGuire <brendan@bren.app>
2025-03-02 01:45:11 -0600

    Prevent the menu reopening (once more, with feeling)

--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -1,7 +1,6 @@
 import { useCallback, useId, useState } from "react";
 import { Button, ButtonProps } from "./Button";
 import { Dialog } from "./Dialog";
-import { AnimatePresence } from "framer-motion";
 
 export type MenuButtonProps = ButtonProps & {
   menu: React.ReactNode;
@@ -10,48 +9,36 @@ export const MenuButton: React.FC<MenuButtonProps> = ({ menu, ...props }) => {
   const id = useId();
   const [show, setShow] = useState(false);
 
-  const onButtonClick = useCallback(() => {
-    setShow((s) => !s);
-  }, []);
-
-  const onDialogPointerDown = useCallback(
-    (e: React.PointerEvent<HTMLDialogElement>) => {
-      e.stopPropagation();
-      if (e.currentTarget === e.target) {
-        e.preventDefault();
-        setShow(false);
-      }
+  const onButtonClick = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.currentTarget.blur();
+      setShow((s) => !s);
     },
     []
   );
 
   return (
     <div className="relative">
-      <AnimatePresence>
-        {show ? (
-          <Dialog
-            aria-modal="true"
-            mode="modal"
-            id={id}
-            open={show}
-            initial={{ opacity: 0, y: 10 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: 10 }}
-            transition={{ duration: 0.05 }}
-            className="fixed md:max-w-full w-screen max-w-[unset] bottom-0 left-0 right-0 p-4 rounded-t-md bg-zinc-900 text-zinc-100 z-50 rounded-b-none mb-0"
-            style={{ maxHeight: "unset", height: "max-content" }}
-            onClose={() => setShow(false)}
-            onPointerDown={onDialogPointerDown}
-          >
-            {menu}
-          </Dialog>
-        ) : null}
-      </AnimatePresence>
+      <Dialog
+        mode="modal"
+        id={id}
+        open={show}
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 10 }}
+        transition={{ duration: 0.05 }}
+        className="fixed md:max-w-full w-screen max-w-[unset] bottom-0 left-0 right-0 p-4 rounded-t-md bg-zinc-900 text-zinc-100 z-50 rounded-b-none mb-0"
+        style={{ maxHeight: "unset", height: "max-content" }}
+        onClose={() => setShow(false)}
+        onClick={() => setShow(false)}
+      >
+        {menu}
+      </Dialog>
       <Button
         {...props}
         aria-haspopup="menu"
         aria-controls={id}
-        onPointerDown={onButtonClick}
+        onClick={onButtonClick}
       />
     </div>
   );

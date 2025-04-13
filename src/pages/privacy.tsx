f173eb5c5feab37bfeeec1ab1436f84746692101
Brendan McGuire <brendan@bren.app>
2025-04-13 16:57:18 -0500

    Fix styling for privacy policy

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -4,7 +4,7 @@ import "./markdown.css";
 
 export const PrivacyPage: React.FC = () => {
   return (
-    <main className="max-w-prose md:max-w-screen-md lg:max-w-screen-lg mx-auto px-4 py-8 overflow-y-auto">
+    <main className="max-w-prose md:max-w-screen-md lg:max-w-screen-lg mx-auto px-4 py-8 overflow-y-auto markdown mt-4">
       <PrivacyPolicy />
     </main>
   );

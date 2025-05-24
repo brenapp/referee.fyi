2421d931dbcce20045e1809fe7e868eb3fb0613b
Brendan McGuire <brendan@bren.app>
2025-05-23 23:04:58 -0500

    Programmatically generate rules schema from zod types

--- lib/rules/main.ts
+++ lib/rules/main.ts
@@ -4,9 +4,11 @@ import { ProgramAbbr, programs, years } from "robotevents";
 export const ProgramAbbrSchema = z
   .enum(Object.keys(programs) as ProgramAbbr[])
   .meta({
+    id: "ProgramAbbr",
     description: "The program abbreviation",
   });
 export const YearSchema = z.enum(years).meta({
+  id: "Year",
   description: "The programs this game applies to.",
 });
 
@@ -41,13 +43,19 @@ export const RuleGroupSchema = z.object({
 
 export type RuleGroup = z.infer<typeof RuleGroupSchema>;
 
-export const GameSchema = z.object({
-  title: z.string().meta({ description: "The game name." }),
-  season: YearSchema.meta({ description: "The season year (ex. 2025-2026)" }),
-  programs: z.array(ProgramAbbrSchema),
-  ruleGroups: z.array(RuleGroupSchema).meta({
-    description: "The rule groups for this game.",
-  }),
-});
+export const GameSchema = z
+  .object({
+    title: z.string().meta({ description: "The game name." }),
+    season: YearSchema.meta({ description: "The season year (ex. 2025-2026)" }),
+    programs: z.array(ProgramAbbrSchema),
+    ruleGroups: z.array(RuleGroupSchema).meta({
+      description: "The rule groups for this game.",
+    }),
+  })
+  .meta({
+    id: "https://referee.fyi/rules/schema.json",
+    title: "Referee FYI Game Schema",
+    description: "Describes the shape of Referee FYI game descriptions.",
+  });
 
 export type Game = z.infer<typeof GameSchema>;

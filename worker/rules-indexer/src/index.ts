a281a59c3e538d39d355459d8d39c0cf4238b1c1
Brendan McGuire <brendan@bren.app>
2025-05-23 22:46:58 -0500

    rules index init setup

---
+++ worker/rules-indexer/src/index.ts
@@ -0,0 +1,23 @@
+export default {
+	async fetch(req) {
+		const url = new URL(req.url);
+		url.pathname = '/__scheduled';
+		url.searchParams.append('cron', '* * * * *');
+		return new Response(`To test the scheduled handler, ensure you have used the "--test-scheduled" then try running "curl ${url.href}".`);
+	},
+
+	// The scheduled handler is invoked at the interval set in our wrangler.jsonc's
+	// [[triggers]] configuration.
+	async scheduled(event, env, ctx): Promise<void> {
+		// A Cron Trigger can make requests to other endpoints on the Internet,
+		// publish to a Queue, query a D1 Database, and much more.
+		//
+		// We'll keep it simple and make an API call to a Cloudflare API:
+		let resp = await fetch('https://api.cloudflare.com/client/v4/ips');
+		let wasSuccessful = resp.ok ? 'success' : 'fail';
+
+		// You could store this result in KV, write to a D1 Database, or publish to a Queue.
+		// In this template, we'll just log the result:
+		console.log(`trigger fired at ${event.cron}: ${wasSuccessful}`);
+	},
+} satisfies ExportedHandler<Env>;

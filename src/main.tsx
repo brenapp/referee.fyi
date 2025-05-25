d33a9bd79d9de5a576b371addc59d38c445d74e1
Brendan McGuire <brendan@bren.app>
2025-05-25 18:23:02 -0500

    remove comment

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,6 @@ import "./index.css";
 import { routeTree } from "./routeTree.gen";
 const router = createRouter({ routeTree });
 
-// Register the router instance for type safety
 declare module "@tanstack/react-router" {
   interface Register {
     router: typeof router;

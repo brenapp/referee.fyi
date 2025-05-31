64977d76c7c661ae1f12ed15295c285c1e8b874a
Brendan McGuire <brendan@bren.app>
2025-05-31 16:15:36 -0500

    remove unused

--- src/utils/data/state.ts
+++ src/utils/data/state.ts
@@ -1,5 +1,3 @@
-import { UAParser } from "ua-parser-js";
-
 export function getBuildMode() {
   return import.meta.env.VITE_REFEREE_FYI_BUILD_MODE;
 }
@@ -19,22 +17,4 @@ export function isWorldsBuild() {
 
 export function isStandardBuild() {
   return getBuildMode() === "STANDARD";
-}
-
-const userAgent = UAParser();
-
-export function getUserAgent() {
-  return userAgent;
-}
-
-export function isSafariMobile() {
-  const isSafari =
-    userAgent.browser.name === "Safari" ||
-    userAgent.browser.name === "Mobile Safari";
-
-  const mobile =
-    userAgent.device.type === "mobile" || userAgent.device.type === "tablet";
-
-  console.log(userAgent, isSafari, mobile);
-  return isSafari && mobile;
 }

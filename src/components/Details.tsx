9bdf9c66ee9a530eddde7d89b62c47640a3ffcef
Brendan McGuire <brendan@bren.app>
2025-06-13 21:35:59 -0500

    Basic rules reference page

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,19 +1,24 @@
 import { ChevronDownIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
+import { twMerge } from "tailwind-merge";
 
 export const Details: React.FC<React.HTMLProps<HTMLDetailsElement>> = (
   props
-) => <details {...props} />;
+) => <details {...props} className={"group " + props.className} />;
 
 export const Summary: React.FC<React.HTMLProps<HTMLElement>> = (props) => (
   <summary
     {...props}
-    className="flex gap-2 items-center active:bg-zinc-700 max-w-full mt-0 sticky top-0 bg-zinc-900 h-16 z-10"
-  >
-    {props.open ? (
-      <ChevronDownIcon height={16} width={16} className="flex-shrink-0" />
-    ) : (
-      <ChevronRightIcon height={16} width={16} className="flex-shrink-0" />
+    className={twMerge(
+      "flex gap-2 items-center active:bg-zinc-700 max-w-full mt-0 sticky top-0 bg-zinc-900",
+      props.className
     )}
+  >
+    <span className="group-open:inline hidden mx-4">
+      <ChevronDownIcon height={20} width={20} className="flex-shrink-0" />
+    </span>
+    <span className="group-open:hidden block mx-4">
+      <ChevronRightIcon height={20} width={20} className="flex-shrink-0" />
+    </span>
     {props.children}
   </summary>
 );
---
+++ src/routes/rules/$program/$year.tsx
@@ -0,0 +1,101 @@
+import { createFileRoute, redirect } from "@tanstack/react-router";
+import { YearSchema, ProgramAbbrSchema, Game } from "@referee-fyi/rules";
+import { queryClient } from "~utils/data/query";
+import { getUseRulesForSeasonQueryParams } from "~utils/hooks/rules";
+import { LinkButton } from "~components/Button";
+import { IconLabel, Input } from "~components/Input";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
+
+type RuleGroupProps = {
+  ruleGroup: Game["ruleGroups"][number];
+  query?: string;
+};
+
+const RuleGroup: React.FC<RuleGroupProps> = ({ ruleGroup, query }) => {
+  const rules = ruleGroup.rules.filter((rule) => {
+    return query
+      ? rule.description.toLowerCase().includes(query.toLowerCase())
+      : true;
+  });
+
+  if (rules.length < 1) {
+    return null;
+  }
+
+  return (
+    <section>
+      <h2 className="font-bold">{ruleGroup.name}</h2>
+      <ol>
+        {rules.map((rule) => (
+          <li key={rule.rule} className="mb-2">
+            <LinkButton from="/rules/$program/$year" className="w-full mt-2">
+              <strong className="text-sm font-mono text-emerald-400">
+                {rule.rule}
+              </strong>
+              <p className="font-normal">{rule.description}</p>
+            </LinkButton>
+          </li>
+        ))}
+      </ol>
+    </section>
+  );
+};
+
+export const RulebookPage: React.FC = () => {
+  const [query, setQuery] = useState("");
+  const { rules } = Route.useLoaderData();
+  return (
+    <main className="mt-4 flex flex-col gap-4 max-h-screen">
+      <section className="overflow-auto flex-1">
+        <h1 className="font-bold text-lg">
+          {rules.title} •{" "}
+          <span className="font-normal font-mono text-sm text-emerald-400">
+            {rules.programs.join("/")} {rules.season}
+          </span>
+        </h1>
+        <div className="mt-4">
+          {rules.ruleGroups.map((group) => (
+            <RuleGroup key={group.name} ruleGroup={group} query={query} />
+          ))}
+        </div>
+      </section>
+      <nav>
+        <IconLabel icon={<MagnifyingGlassIcon height={24} />}>
+          <Input
+            placeholder="Search rules and Q&As..."
+            className="flex-1"
+            value={query}
+            onChange={(e) => setQuery(e.currentTarget.value)}
+          />
+        </IconLabel>
+      </nav>
+    </main>
+  );
+};
+
+export const Route = createFileRoute("/rules/$program/$year")({
+  component: RulebookPage,
+  loader: async ({ params }) => {
+    const program = ProgramAbbrSchema.safeParse(params.program);
+    const year = YearSchema.safeParse(params.year);
+
+    if (!program.success || !year.success) {
+      throw redirect({ to: "/", from: "/rules/$program/$year" });
+    }
+
+    const rules = await queryClient.ensureQueryData(
+      getUseRulesForSeasonQueryParams(program.data, year.data)
+    );
+
+    if (!rules) {
+      throw redirect({ to: "/", from: "/rules/$program/$year" });
+    }
+
+    return {
+      program: program.data,
+      year: year.data,
+      rules,
+    };
+  },
+});

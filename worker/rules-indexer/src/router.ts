b5803293b403fb668edd60f793479e952ef60c8e
Brendan McGuire <brendan@bren.app>
2025-08-03 15:48:17 -0500

    rules-indexer cleanup

--- worker/rules-indexer/src/router.ts
+++ worker/rules-indexer/src/router.ts
@@ -1,4 +1,4 @@
-import { z, ZodType } from "zod/v4";
+import { z } from "zod/v4";
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { swaggerUI } from "@hono/swagger-ui";
 import { cors } from "hono/cors";
@@ -15,18 +15,6 @@ export const ErrorResponseSchema = z
     id: "ErrorResponse",
     description: "Error response schema",
   });
-
-export const SuccessResponseSchema = (data: ZodType) =>
-  z.object({
-    success: z.literal(true),
-    data,
-  });
-
-export const ResponseSchemaTemplate = (data: ZodType) =>
-  z.discriminatedUnion("success", [
-    ErrorResponseSchema,
-    SuccessResponseSchema(data),
-  ]);
 
 export const app = new OpenAPIHono<{ Bindings: Env }>();
 

e21da7245140d9b7877b2176d0e3e5adf49059fc
Brendan McGuire <brendan@bren.app>
2025-08-26 00:45:09 -0500

    Return info about image asset

--- worker/sync/src/api/integration/v1/$sku/asset.ts
+++ worker/sync/src/api/integration/v1/$sku/asset.ts
@@ -8,6 +8,8 @@ import {
   VerifyUserAssetAuthorizedQuerySchema,
 } from "../../../../utils/verify";
 import { createRoute } from "@hono/zod-openapi";
+import { signAssetUrl } from "../../../../utils/crypto";
+import { AssetTypeSchema } from "@referee-fyi/share";
 
 export const ParamsSchema = VerifyIntegrationTokenParamsSchema;
 export const QuerySchema = z.object({
@@ -18,7 +20,13 @@ export const QuerySchema = z.object({
 export const SuccessResponseSchema = z
   .object({
     success: z.literal(true),
-    data: z.object({}),
+    data: z.object({
+      type: AssetTypeSchema,
+      owner: z.string().optional(),
+      sku: z.string(),
+      url: z.url(),
+      expires_at: z.string(),
+    }),
   })
   .meta({
     id: "GetIntegrationV1AssetResponse",
@@ -63,10 +71,54 @@ app.openapi(route, async (c) => {
     );
   }
 
+  const verifyUserAssetAuthorized = c.get("verifyUserAssetAuthorized");
+  if (!verifyUserAssetAuthorized) {
+    return c.json(
+      {
+        success: false,
+        error: {
+          name: "ValidationError",
+          message: "You are not authorized to access this asset.",
+        },
+        code: "VerifyUserAssetAuthorizedUserNotAuthorized",
+      } as const satisfies z.infer<typeof ErrorResponseSchema>,
+      400
+    );
+  }
+
+  const url = verifyUserAssetAuthorized.image.variants?.find((variant) =>
+    variant.endsWith("public")
+  );
+  if (!url) {
+    return c.json(
+      {
+        success: false,
+        error: {
+          name: "ValidationError",
+          message: "Could not find a public asset variant.",
+        },
+        code: "VerifyUserAssetAuthorizedImageNotFound",
+      } as const satisfies z.infer<typeof ErrorResponseSchema>,
+      400
+    );
+  }
+
+  const signed = await signAssetUrl(
+    url,
+    c.env.CLOUDFLARE_IMAGES_SIGNATURE_TOKEN,
+    60 * 5
+  );
+
   return c.json(
     {
       success: true,
-      data: {},
+      data: {
+        type: "image",
+        owner: verifyUserAssetAuthorized.asset.owner,
+        sku: c.req.param("sku"),
+        url: signed.toString(),
+        expires_at: new Date(Date.now() + 1000 * 60 * 5).toISOString(),
+      },
     } as const satisfies z.infer<typeof SuccessResponseSchema>,
     200
   );

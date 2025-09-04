d0178624736b23f4ab49df96c34947cf75e007c8
Brendan McGuire <brendan@bren.app>
2025-09-03 22:19:34 -0500

    Add robotevents cache

---
+++ worker/events/src/index.ts
@@ -0,0 +1,110 @@
+import { join } from "path";
+import { Context, Hono } from "hono";
+import { ContentfulStatusCode } from "hono/utils/http-status";
+
+const ROBOTEVENTS_URL = "https://www.robotevents.com/api/v2";
+
+const prefixes = ["/api/v2", "/api/events"];
+const routes = [
+  "/events",
+  "/events/{id}",
+  "/events/{id}/teams",
+  "/events/{id}/skills",
+  "/events/{id}/awards",
+  "/events/{id}/divisions/{div}/matches",
+  "/events/{id}/divisions/{div}/finalistRankings",
+  "/events/{id}/divisions/{div}/rankings",
+  "/teams",
+  "/teams/{id}",
+  "/teams/{id}/events",
+  "/teams/{id}/matches",
+  "/teams/{id}/rankings",
+  "/teams/{id}/skills",
+  "/teams/{id}/awards",
+  "/programs/{id}",
+  "/programs",
+  "/seasons",
+  "/seasons/{id}",
+  "/seasons/{id}/events",
+];
+
+const blockedQueryKeys = new Set(["myEvents", "myTeams"]);
+function canUseCache(url: URL): boolean {
+  return ![...url.searchParams.keys()].some((key) => blockedQueryKeys.has(key));
+}
+
+function getCanonicalResourceUrl(url: URL): URL | null {
+  const pathname = url.pathname;
+  for (const prefix of prefixes) {
+    if (pathname.startsWith(prefix)) {
+      const newPath = pathname.replace(prefix, "");
+      const newUrl = new URL(ROBOTEVENTS_URL + newPath);
+      newUrl.search = url.search;
+      return newUrl;
+    }
+  }
+
+  return null;
+}
+type AppArgs = {
+  Bindings: Env;
+};
+
+async function handle(c: Context<AppArgs>) {
+  const url = getCanonicalResourceUrl(new URL(c.req.url));
+  if (!url) {
+    return c.json({ error: "Not Found", code: 404 }, 404);
+  }
+
+  const token =
+    c.req.header("Authorization")?.replace("Bearer ", "") ??
+    (await c.env.ROBOTEVENTS_TOKEN.get());
+
+  const cached = canUseCache(url)
+    ? await c.env.robotevents.get(url.toString(), "json")
+    : null;
+
+  if (cached) {
+    return c.json(cached, 200);
+  }
+
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!response.ok) {
+    const json = await response.json().catch(() => null);
+    return c.json(
+      json ?? { error: response.statusText, code: response.status },
+      response.status as ContentfulStatusCode
+    );
+  }
+
+  const data = await response.json();
+
+  if (canUseCache(url)) {
+    c.env.robotevents.put(url.toString(), JSON.stringify(data), {
+      expirationTtl: 60 * 5,
+    });
+  }
+
+  return c.json(data, 200);
+}
+
+const app = new Hono<{ Bindings: Env }>();
+
+const endpoints = prefixes.flatMap((prefix) =>
+  routes.map((route) => join(prefix, route))
+);
+
+for (const endpoint of endpoints) {
+  app.get(endpoint, handle);
+}
+
+app.all("*", (c) => c.json({ error: "Not Found", code: 404 }, 404));
+
+export default {
+  ...app,
+} satisfies ExportedHandler<Env>;

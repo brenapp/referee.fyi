a446c6f2aa888856026352fdf82ea8955c0326e9
Brendan McGuire <brendan@bren.app>
2026-01-04 22:19:37 -0500

    move client to apps

--- apps/referee-fyi/src/routes/rules/$program/$year.tsx
+++ apps/referee-fyi/src/routes/rules/$program/$year.tsx
@@ -1,5 +1,5 @@
 import { Await, createFileRoute, redirect } from "@tanstack/react-router";
-import { Game } from "../../../../lib/rules/src/main";
+import type { Game } from "@referee-fyi/rules";
 import { queryClient } from "~utils/data/query";
 import { getUseRulesForSeasonQueryParams } from "~utils/hooks/rules";
 import { ExternalLinkButton } from "~components/Button";
